b3f0451ddce1dbb6651f41c007110b1a6507b285
xiaoleixin <xiaoleixin123@gmail.com>
2021-09-29 15:17:03 +0800

    modular

---
+++ 02_CommonJS-Node/modules/module3.js
@@ -0,0 +1,12 @@
+
+exports.peiqi = [1,3,5,7,9]
+
+/*问题:
+  1.暴露的本质到底是什么?modules.exports所指向的那个对象
+  2.在CommonJs模块规范中，modules.exports 和 exports.xxx 不能混用。
+  3.如果混用，以module.exports为主*/
+/*modules.exports = function haha() {
+  console.log('哈哈')
+}*/
+
+
